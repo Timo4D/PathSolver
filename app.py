059327421fcfcb73135493e4c339961e3902ae63
Timo Gerstenhauer <timo.gerstenhauer@gmx.de>
2024-09-17 14:40:46 +0200

    first simple graph

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from shiny import App, render, ui
+import matplotlib.pyplot as plt
+import numpy as np
 
 example_page =  ui.page_fluid(
     ui.panel_title("Djikstra Shiny!"),
@@ -6,10 +8,13 @@ example_page =  ui.page_fluid(
     ui.output_text_verbatim("txt"),
 )
 
+simple_plot = ui.page_fluid(
+        ui.output_plot("plot")
+        )
 
 app_ui = ui.page_navbar(
         ui.nav_panel("Startseite", example_page),
-        ui.nav_panel("Über den Algorithmus", "Page Über den Algorithmus"),
+        ui.nav_panel("Über den Algorithmus", ui.output_plot("plot")),
         ui.nav_panel("Über das Projekt", "Page Über das Projekt"),
         title = "Djikstra",
         id = "navbar",
@@ -18,6 +23,25 @@ app_ui = ui.page_navbar(
 
 
 def server(input, output, session):
+
+    @render.plot
+    def plot():
+        
+        plt.style.use('_mpl-gallery')
+
+# make data
+        np.random.seed(1)
+        x = 4 + np.random.normal(0, 1.5, 200)
+
+# plot:
+        fig, ax = plt.subplots()
+
+        ax.hist(x, bins=8, linewidth=0.5, edgecolor="white")
+
+        ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+               ylim=(0, 56), yticks=np.linspace(0, 56, 9))
+        plt.show()
+
     @render.text
     def txt():
         return f"n*2 is {input.n() * 2}"
