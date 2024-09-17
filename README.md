169b9563334bfb575f8852e5dab2532f85c009b8
Timo Gerstenhauer <timo.gerstenhauer@gmx.de>
2024-09-17 13:12:28 +0200

    added navbar

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Dijkstra
 A Simple Python Shiny app to visualize the Dijkstra-Algorithm
+
+# Install
+pip install --upgrade pip wheel shiny htmltools shinyswatch
+
+
+# Run
+shiny run --reload --launch-browser app.py
--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from shiny import App, render, ui
 
-app_ui = ui.page_fluid(
-    ui.panel_title("Hello Shiny!"),
+example_page =  ui.page_fluid(
+    ui.panel_title("Djikstra Shiny!"),
     ui.input_slider("n", "N", 0, 100, 20),
     ui.output_text_verbatim("txt"),
 )
+
+
+app_ui = ui.page_navbar(
+        ui.nav_panel("Startseite", example_page),
+        ui.nav_panel("Über den Algorithmus", "Page Über den Algorithmus"),
+        ui.nav_panel("Über das Projekt", "Page Über das Projekt"),
+        title = "Djikstra",
+        id = "navbar",
+        )
+
 
 
 def server(input, output, session):
