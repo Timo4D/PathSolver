353938915901335f964b25eca4252d2113cf7fbd
Timo Gerstenhauer <timo.gerstenhauer@gmx.de>
2024-09-23 16:17:35 +0200

    switch between random and predefined graph
    Signed-off-by: Timo Gerstenhauer <timo.gerstenhauer@gmx.de>

--- modules/graph_ui.py
+++ modules/graph_ui.py
@@ -1,6 +1,12 @@
+from enum import Enum
+
 import networkx as nx
 from shiny import ui, render
 
+
+class GraphType(Enum):
+    RANDOM_GRAPH = "random_graph"
+    KOOT_EXAMPLE_DEUTSCHLAND = "koot_example_deutschland"
 
 def graph_ui():
     return ui.page_fluid(
@@ -9,16 +15,15 @@ def graph_ui():
                 ui.input_selectize(
                     "selectize_graph",
                     "Select a Graph",
-                    {"random": "Random Graph", "koot_beispiel_deutschland": "Deutschland Beispiel"}
+                    {GraphType.RANDOM_GRAPH.value: "Random Graph",
+                     GraphType.KOOT_EXAMPLE_DEUTSCHLAND.value: "Deutschland Beispiel"}
                 ),
-                ui.input_slider("n_slider", "Number of Nodes", 1, 30, 8),
-                ui.input_slider("k_slider", "K", 2, 5, 3),
-                ui.input_slider("p_slider", "P", 0, 1, 0.5),
+                ui.output_ui("random_graph_sliders"),
                 ui.input_numeric("start_node", "Start Node", value=1),
                 ui.input_numeric("target_node", "Target Node", value=2),
                 ui.input_numeric("layout_seed", "Layout Seed", value=1),
             ),
-            ui.output_plot("graph_plot")
+            ui.output_ui("display_graph")
         ),
     )
 
@@ -28,7 +33,7 @@ def generate_random_graph(n, k, p):
     return G
 
 
-def draw_random_graph(G, pos, start, target):
+def draw_graph(G, pos, start, target, weights=None):
     nx.draw_networkx_labels(G, pos)
 
     color_map = []
@@ -42,17 +47,58 @@ def draw_random_graph(G, pos, start, target):
 
     nx.draw_networkx_edges(G, pos)
     nx.draw_networkx_nodes(G, pos, node_color=color_map)
+    edge_labels = nx.get_edge_attributes(G, 'weight')
+
+    # Draw the weight of the edges
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
 
 
 def graph_ui_server(input, output, session):
     graph_data = {
-        "graph": None,
+        "graph": nx.Graph(),
         "layout_seed": None,
     }
 
     @output
+    @render.ui
+    def display_graph():
+        """Controls which graph is shown."""
+        return ui.output_plot(input.selectize_graph())
+
+    @output
+    @render.ui
+    def random_graph_sliders():
+        if input.selectize_graph() == GraphType.RANDOM_GRAPH.value:
+            return ui.TagList(
+                ui.input_slider("n_slider", "Number of Nodes", 1, 30, 8),
+                ui.input_slider("k_slider", "K", 2, 5, 3),
+                ui.input_slider("p_slider", "P", 0, 1, 0.5),
+            )
+
+    @output
     @render.plot
-    def graph_plot():
+    def koot_example_deutschland():
+        edges = [
+            (0, 6, 35), (0, 7, 224), (1, 2, 291), (1, 3, 128), (1, 4, 137),
+            (2, 4, 292), (3, 5, 99), (3, 7, 112), (4, 6, 270), (5, 7, 151)
+        ]
+
+        G = nx.Graph()
+        for u, v, d in edges:
+            G.add_edge(u, v, weight=d)
+
+        pos = nx.spring_layout(G, seed=input.layout_seed())
+        draw_graph(
+            G,
+            pos,
+            input.start_node(),
+            input.target_node(),
+        )
+
+
+    @output
+    @render.plot
+    def random_graph():
         redo_layout = False
         if (graph_data["graph"] is None
                 or input.n_slider() != graph_data.get("n")
@@ -74,7 +120,7 @@ def graph_ui_server(input, output, session):
             graph_data["pos"] = nx.spring_layout(graph_data["graph"], seed=input.layout_seed())
             graph_data["layout_seed"] = input.layout_seed()
 
-        draw_random_graph(
+        draw_graph(
             graph_data["graph"],
             graph_data["pos"],
             input.start_node(),
