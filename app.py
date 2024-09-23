6c348e52a5fe21196b1077fc86eaa7b9f4cc1e8e
Timo Gerstenhauer <timo.gerstenhauer@gmx.de>
2024-09-23 15:40:01 +0200

    move graph_ui server code to graph_ui.py
    Signed-off-by: Timo Gerstenhauer <timo.gerstenhauer@gmx.de>

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-import networkx as nx
 from shiny import App, render, ui
 
-from modules.graph_ui import graph_ui, draw_random_graph, generate_random_graph
+from modules.graph_ui import graph_ui, graph_ui_server
 from modules.plot import create_plot
 from modules.simple_graph import create_graph
 
@@ -29,10 +28,7 @@ app_ui = ui.page_navbar(
 
 
 def server(input, output, session):
-    graph_data = {
-        "graph": None,
-        "layout_seed": None,
-    }
+    graph_ui_server(input, output, session)
 
     @render.plot
     def plot():
@@ -42,37 +38,6 @@ def server(input, output, session):
     @render.plot
     def graph():
         return create_graph()
-
-    @output
-    @render.plot
-    def graph_plot():
-        redo_layout = False
-        if (graph_data["graph"] is None
-                or input.n_slider() != graph_data.get("n")
-                or input.k_slider() != graph_data.get("k")
-                or input.p_slider() != graph_data.get("p")):
-            graph_data["graph"] = generate_random_graph(
-                input.n_slider(),
-                input.k_slider(),
-                input.p_slider(),
-            )
-            graph_data["n"] = input.n_slider()
-            graph_data["k"] = input.k_slider()
-            graph_data["p"] = input.p_slider()
-            redo_layout = True
-
-        if (graph_data["layout_seed"] is None
-                or input.layout_seed() != graph_data["layout_seed"]
-                or redo_layout):
-            graph_data["pos"] = nx.spring_layout(graph_data["graph"], seed=input.layout_seed())
-            graph_data["layout_seed"] = input.layout_seed()
-
-        draw_random_graph(
-            graph_data["graph"],
-            graph_data["pos"],
-            input.start_node(),
-            input.target_node(),
-        )
 
 
 app = App(app_ui, server)
--- modules/graph_ui.py
+++ modules/graph_ui.py
@@ -1,11 +1,16 @@
 import networkx as nx
-from shiny import ui
+from shiny import ui, render
 
 
 def graph_ui():
     return ui.page_fluid(
         ui.layout_sidebar(
             ui.sidebar(
+                ui.input_selectize(
+                    "selectize_graph",
+                    "Select a Graph",
+                    {"random": "Random Graph", "koot_beispiel_deutschland": "Deutschland Beispiel"}
+                ),
                 ui.input_slider("n_slider", "Number of Nodes", 1, 30, 8),
                 ui.input_slider("k_slider", "K", 2, 5, 3),
                 ui.input_slider("p_slider", "P", 0, 1, 0.5),
@@ -37,3 +42,41 @@ def draw_random_graph(G, pos, start, target):
 
     nx.draw_networkx_edges(G, pos)
     nx.draw_networkx_nodes(G, pos, node_color=color_map)
+
+
+def graph_ui_server(input, output, session):
+    graph_data = {
+        "graph": None,
+        "layout_seed": None,
+    }
+
+    @output
+    @render.plot
+    def graph_plot():
+        redo_layout = False
+        if (graph_data["graph"] is None
+                or input.n_slider() != graph_data.get("n")
+                or input.k_slider() != graph_data.get("k")
+                or input.p_slider() != graph_data.get("p")):
+            graph_data["graph"] = generate_random_graph(
+                input.n_slider(),
+                input.k_slider(),
+                input.p_slider(),
+            )
+            graph_data["n"] = input.n_slider()
+            graph_data["k"] = input.k_slider()
+            graph_data["p"] = input.p_slider()
+            redo_layout = True
+
+        if (graph_data["layout_seed"] is None
+                or input.layout_seed() != graph_data["layout_seed"]
+                or redo_layout):
+            graph_data["pos"] = nx.spring_layout(graph_data["graph"], seed=input.layout_seed())
+            graph_data["layout_seed"] = input.layout_seed()
+
+        draw_random_graph(
+            graph_data["graph"],
+            graph_data["pos"],
+            input.start_node(),
+            input.target_node(),
+        )
