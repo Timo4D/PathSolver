3a8b19f51db08cdfeb228bb19de24256ae3311d4
Timo Gerstenhauer <timo.gerstenhauer@gmx.de>
2025-07-12 16:50:15 +0200

    start of rewrite

--- app.py
+++ app.py
@@ -1,33 +1,16 @@
-from shiny import App, ui
+from shiny import App, render, ui
 
-from modules.dijkstra_info import dijkstra_info
-from modules.graph_ui import graph_ui, graph_ui_server
-from modules.project_information import project_information
-
-example_page = ui.page_fluid(
-    ui.panel_title("Djikstra Shiny!"),
+app_ui = ui.page_fluid(
+    ui.panel_title("Hello Shiny!"),
     ui.input_slider("n", "N", 0, 100, 20),
     ui.output_text_verbatim("txt"),
 )
 
-simple_plot = ui.page_fluid(
-    ui.output_plot("plot")
-)
-
-simple_graph = ui.page_fluid(
-    ui.output_plot("graph")
-)
-
-app_ui = ui.page_navbar(
-    ui.nav_panel("Start", graph_ui()),
-    ui.nav_panel("About the Project", project_information),
-    ui.nav_panel("More about the Dijkstra-Algorithm", dijkstra_info),
-    title="PathSolver by Timo Gerstenhauer"
-)
-
 
 def server(input, output, session):
-    graph_ui_server(input, output, session)
+    @render.text
+    def txt():
+        return f"n*2 is {input.n() * 2}"
 
 
 app = App(app_ui, server)
